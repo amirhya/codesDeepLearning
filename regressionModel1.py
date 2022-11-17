ddbad0ef92e8fdad05c041e901dec3d7d52cac35
amir.hya <yazdani.ahya@gmail.com>
2022-11-16 20:31:41 -0500

    added tensorboard and cleaner regression model

--- regressionModel1.py
+++ regressionModel1.py
@@ -5,56 +5,83 @@ import matplotlib.pyplot as plt
 import functions
 import tensorflow as tf
 from tensorflow import keras
+import tensorboard_m
 
 data = fetch_california_housing()
 
 X, XTest, y, yTest = train_test_split(data.data, data.target)
-XTrain, XValid, yTrain,yValid= train_test_split(X, y)
+XTrain, XValid, yTrain, yValid = train_test_split(X, y)
 
-scaler=StandardScaler()
+scaler = StandardScaler()
 scaler.fit(XTrain)
-XTrain=scaler.transform(XTrain)
-XTest=scaler.transform(XTest)
-XValid=scaler.transform(XValid)
+XTrain = scaler.transform(XTrain)
+XTest = scaler.transform(XTest)
+XValid = scaler.transform(XValid)
 
 
-
-class myModelSequential():
+class ModelSequential:
     def __init__(self, input_shape):
         self.model = keras.models.Sequential()
-        self.input_shape=input_shape
-    def build(self):
+        self.input_shape = input_shape
+
+    def build(self, kernelInitializer="he_normal", numNeurons=10,numLayers=2):
         self.model.add(keras.layers.Input(shape=self.input_shape))
-        self.model.add(keras.layers.Dense(300,activation='relu', kernel_initializer= "he_normal"))
-        self.model.add(keras.layers.Dense(100, activation=keras.activations.relu,kernel_initializer= "he_normal"))
+        for _ in range(numLayers):
+            self.model.add(keras.layers.Dense(numNeurons))
+            #self.model.add(keras.layers.BatchNormalization())
+            self.model.add(keras.layers.Activation("relu"))
+            self.model.add(keras.layers.Dropout(0.3))
+
         self.model.add(keras.layers.Dense(1, activation='linear'))
-    def compile(self, learningRate=0.001):
-        self.model.compile(loss="mse",
-                           optimizer=keras.optimizers.Adam(lr=learningRate))
+
+    def compile(self, loss, learningRate=0.001):
+        self.model.compile(loss=loss,
+                           optimizer=keras.optimizers.Adam(learning_rate=learningRate))
+
     def fit(self, XTrain, yTrain, XValid, yValid, callbacks=[], **kwargs):
-        history=self.model.fit(XTrain,yTrain,validation_data=(XValid,yValid),callbacks=callbacks,**kwargs)
+        history = self.model.fit(XTrain, yTrain, validation_data=(XValid, yValid), callbacks=callbacks, **kwargs)
         return history
-    def evaluate(self,X, y):
-        return self.model.evaluate(X,y)
-    def predict(self,X):
+
+    def evaluate(self, X, y):
+        return self.model.evaluate(X, y)
+
+    def predict(self, X):
         return self.model.predict(X)
-    def getModel(self):
+
+    def get_model(self):
         return self.model
 
 
-model=myModelSequential(XTrain.shape[1:])
-model.build()
+loss=tf.keras.losses.Huber()
+model = ModelSequential(XTrain.shape[1:])
+model.build(numNeurons=128,numLayers=4)
 
-rawModel=model.getModel()
-lrHistory=functions.adjustLearningRate(X=XTrain,y=yTrain, model=rawModel, numEpochs=500, minLearningRate=1e-5, maxLearningRate=10,
-                       optimizer=keras.optimizers.Adam(), loss='mse',metric='mae')
-
-model.compile()
-
-
-history=model.fit(XTrain,yTrain, XValid, yValid, epochs=100)
-yPred= model.predict(XTest)
-plt.plot(yPred-yTest,label='pred')
-plt.show()
+rawModel = model.get_model()
+# lrHistory = functions.adjust_learning_rate(X=XTrain, y=yTrain, model=rawModel, numEpochs=200, minLearningRate=1e-6,
+#                                            maxLearningRate=1, optimizer=keras.optimizers.Adam(),
+#                                            loss=loss,
+#                                            metric='mae')
+#
+# functions.plot_history(lrHistory, type="learning rate", limits=[1e-6, 1, 0, 0.5])
 
 
+model.compile(learningRate=1e-3, loss=loss)
+tensorboardCB= tensorboard_m.TensorBoardCB(modelName='sequemtialRegression').getCB()
+checkpointCallBack=keras.callbacks.ModelCheckpoint("models/imageClassificationModel.h5", save_best_only=True)
+
+history = model.fit(XTrain, yTrain, XValid, yValid, epochs=100, callbacks=[tensorboardCB])
+model.evaluate(XTest,yTest)
+
+
+# #
+
+# functions.plot_history(history)
+# yPred = model.predict(XTest)
+# plt.plot(yPred - yTest, label='pred')
+# plt.show()
+#
+# fig, ax = plt.subplots(figsize=(10, 7))
+# ax.hist(yPred)
+#
+# # Show plot
+# plt.show()
